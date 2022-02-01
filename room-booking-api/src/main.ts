bec8c704885e4d154c59aea172911539f7b1b164
M0lik <guillaume.verneret@gmail.com>
2022-02-01 15:21:32 +0100

    [fix] port

--- room-booking-api/src/main.ts
+++ room-booking-api/src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  const port = parseInt(3000);
+  const port = 3000;
   await app.listen(port);
 }
 
